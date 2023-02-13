93c68e8207f42ec52cfb0c16303c8d7a4547ab93
mer1984calovini <m_erchu24@live.com>
2023-02-13 09:45:22 -0300

    cambios

--- Pre entrega 2 Calovini JS/main.js
+++ Pre entrega 2 Calovini JS/main.js
@@ -22,15 +22,15 @@ class Vehiculo{
         }
 }
 
-const peogeot = new Vehiculo( "peogeot",208, 2022, 100000, 6000000);
-const citroen = new Vehiculo( "citroen", 2011, 500000, 500000,2000000); 
+const peogeot = new Vehiculo("peogeot",208, 2022, 100000, 6000000);
+const citroen = new Vehiculo("citroen", 2011, 500000, 500000,2000000); 
 const fiat = new Vehiculo("fiat","uno", 2013, 30000, 2500000);    
 const chevrolet = new Vehiculo("chevrolet","prisma", 2017, 50000, 4000000);
 const renault = new Vehiculo("renault","sandero", 2020, 19000, 4500000);
 
 console.log(peogeot,citroen,fiat,chevrolet,renault);
 
-const vehiculos=["peogeot", "citroen", "fiat", "chevrolet", "renault"];
+const vehiculos=[peogeot, citroen, fiat, chevrolet, renault];
 console.log( vehiculos);
 
 
@@ -38,21 +38,19 @@ console.log( vehiculos);
 let auto=prompt("ingrese el nombre del auto que quiere adquirir: ")
 
 const busqueda=vehiculos.find(Vehiculo => Vehiculo.nombre === auto);
+console.log(busqueda);
 alert( "el auto que quiere adquirir se encuentra en nuestro de stock de vehiculos");
 
-/*auto= vehiculos.includes();
-console.log(auto);
-while (auto == true){
-    let medioDePago= prompt("ingrese su forma de pago: efectivo/ financiado");
-        switch(medioDePago) {
-//         // vehiculos.forEach(auto => alert ("el pecio es de " auto.precio) 
 
-//         // }*/
-/*  case "efectivo":
-            alert( "El valor es: " auto.precio );
-            break; 
-    case "financiado":
-            alert("El valor es: " (auto + (this.precio + Financiacion)));
-            break;
-    }
-} */
+
+
+
+
+
+
+
+
+
+
+
+
